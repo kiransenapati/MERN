0a788a997b8a668ac6a653f15e3eea806282ae16
Chandra Kiran Senapati <kiran-chandra.senapati@hpe.com>
2022-04-03 14:57:55 +0530

    Backend Code complete
    Node and Express backend code for the restaurant reviews

---
+++ Resturant_review_using_MERN/src/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Switch, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddReview from "./components/add-review";
+import Restaurant from "./components/restaurant";
+import Restaurants from "./components/restaurants-list";
+import Login from "./components/login";
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
